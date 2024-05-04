7b2d0e27bc6256bda5bcc6352d3a037ee6444156
JeremyCPE <mohariry.jeremy@gmail.com>
2024-05-04 14:17:27 +0900

    (feat) add Services component

---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Creation .NET application",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+    {
+        s_no:"02",
+        s_name:"Design application",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+    {
+        s_no:"03",
+        s_name:"Work in agile",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+    {
+        s_no:"04",
+        s_name:"Software developpement",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+    {
+        s_no:"05",
+        s_name:"DevOps",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+    {
+        s_no:"06",
+        s_name:"Scrum Master",
+        s_desc:"Lorem ipsum Lorem ipsum Lorem Ipsum..."
+    },
+]
+
+export default Services_Data
