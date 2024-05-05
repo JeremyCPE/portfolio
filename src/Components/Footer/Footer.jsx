e59f041047d8a07bd78df19c57b1080384f1ab5b
JeremyCPE <mohariry.jeremy@gmail.com>
2024-05-06 00:03:22 +0900

    (feat) add contact and finish mywork

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+//import footer_logo from '../../assets/footer_logo.svg'
+//import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-bottom">
+
+        </div>
+    </div>
+    )
+}
+
+export default Footer
